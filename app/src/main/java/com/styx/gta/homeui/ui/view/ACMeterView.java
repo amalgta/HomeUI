da3ada1f2a92979ad5b6e1096189669ae7036db6
Amal George Thomas <amal.george@flytxt.com>
2016-10-20 19:09:48 +0530

    Nasham.. Sarva Naasham

--- app/src/main/java/com/styx/gta/homeui/ui/view/ACMeterView.java
+++ app/src/main/java/com/styx/gta/homeui/ui/view/ACMeterView.java
@@ -31,24 +31,97 @@ import com.styx.gta.homeui.R;
  * Created by amal.george on 19-10-2016.
  */
 
+/**
+
+ GTA Notes
+
+    Input Expected:
+        MinValue=2
+        MaxValue=200
+        value=100
+        radius=60
+        meterColor=COLOR
+        SweepAngle=45
+        meterValueTextColor
+
+ Rules :
+    MaxValue>=CurrentValue>=MinValue
+    MinValue<MaxValue
+ Constants :
+    MaxDegree=359.9f
+
+ Calculation Variables:
+    ConceptualMax=(MaxValue-MinValue)
+    ConceptualMin=0
+
+    CurrentPercentageFromValue=( (CurrentValue/ConceptualMax) * 100)
+    CurrentValueFromPercentage=( ( (CurrentPercentage/100) * ConceptualMax) ) + MinValue)
+
+
+    initMaxDegree=(SweepAngle/2)
+    initMinDegree=(MaxDegree - (SweepAngle/2))
+    CurrentPercentageFromDegree=( (CurrentDegree*100) / SweepAngle)
+    CurrentConceptualDegreeFromPercentage=( (Percentage*SweepAngle) / 100)
+    CurrentRepresentationalDegree=( (initMinDegree+CurrentConceptualDegreeFromPercentage) % MaxDegree)
+
+ **/
+
+
+
 public class ACMeterView extends View implements OnGestureListener {
     String TAG=getClass().getCanonicalName();
-    //UI Values
-    private String          value;
-    private float           radius;
-    private int             meterColor;
-    private int             meterValueTextColor;
 
-    //Component Values
-    float                   currentDegree;
-    float                   maxDegree;
-    float                   minDegree;
-    //boolean               isDegree; //TODO Add Degree/Fahrenheit Units
+    //UI Dimensions
+    private float          value,minValue,maxValue;
+    private float           radius,sweepAngle;
+    private int             meterColor,meterValueTextColor;
+
+    //Constants
+    private final float maxDegree=359.9f;
+
+    //Intermediary Constants
+    private float conceptualMax;
+    private float initMaxDegree,initMinDegree;
+
+    //Indicatory Variables
+    private float currentOffset=0;
 
     private GestureDetector 	gestureDetector;
     private boolean 			mState = false;
     private float 				mAngleDown , mAngleUp;
 
+    private void initializeConstants(Context mContext, AttributeSet attrs) {
+        //SetState(mState); {Can be used to implement degree/fahrenheit transformation}
+
+        //Defaults
+        final float RADIUS=80.0f;
+        final float SWEEP=120.0f;
+        final float MINVALUE=0f;
+        final float MAXVALUE=100.0f;
+        final int DEFAULT_METER_COLOR=Color.BLACK;
+        final int DEFAULT_METERVALUETEXT_COLOR=Color.WHITE;
+
+        TypedArray a = mContext.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.ACMeterView, 0, 0);
+        try {
+            value = a.getFloat(R.styleable.ACMeterView_value,((MINVALUE+MAXVALUE)/2));
+            minValue = a.getFloat(R.styleable.ACMeterView_minValue,MINVALUE);
+            maxValue = a.getFloat(R.styleable.ACMeterView_maxValue,MAXVALUE);
+            radius = a.getDimension(R.styleable.ACMeterView_radius, RADIUS);
+            sweepAngle = a.getDimension(R.styleable.ACMeterView_sweepAngle, SWEEP);
+            meterColor = a.getColor(R.styleable.ACMeterView_meterColor, DEFAULT_METER_COLOR);
+            meterValueTextColor = a.getColor(R.styleable.ACMeterView_meterValueTextColor, DEFAULT_METERVALUETEXT_COLOR);
+        } finally {
+            a.recycle();
+        }
+        //Validate
+    }
+
+    private void initialCalculations(){
+        conceptualMax=(maxValue-minValue);
+        initMaxDegree=(sweepAngle/2);
+        initMinDegree=(maxDegree-(sweepAngle/2));
+    }
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         if (gestureDetector.onTouchEvent(event)) {
@@ -80,17 +153,20 @@ public class ACMeterView extends View implements OnGestureListener {
         // if we click up the same place where we clicked down, it's just a button press
         if (! Float.isNaN(mAngleDown) && ! Float.isNaN(mAngleUp) && Math.abs(mAngleUp-mAngleDown) < 10) {
             SetState(!mState);
+            //setRotorPosAngle((mAngleDown+mAngleDown)/2);
             if (m_listener != null) m_listener.onStateChange(mState);
         }
         return true;
     }
+
     public void setRotorPosAngle(float deg) {
 
        // if (deg >= 270 || deg <= 180) {
-            if (deg > 180) deg = deg - 360;
-            invalidate();
-            currentDegree=deg;
-            Log.e("GTA",deg+"");//getWidth()/2, getHeight()/2);
+         //   if (deg > 180) deg = deg - maxDegree;
+        currentOffset=deg;
+        invalidate();
+        //    currentPercentage=deg;
+            Log.e("GTA",deg+"");
      //   }
     }
     public void setRotorPercentage(int percentage) {
@@ -110,22 +186,41 @@ public class ACMeterView extends View implements OnGestureListener {
             // instead of getting 0-> 180, -180 0 , we go for 0 -> 360
             float posDegrees = rotDegrees;
             if (rotDegrees < 0)
-                posDegrees = 360 + rotDegrees;
+                posDegrees = maxDegree + rotDegrees;
 
             // deny full rotation, start start and stop point, and get a linear scale
             //if (posDegrees > 210 || posDegrees < 150) {
-                // rotate our imageview
-                setRotorPosAngle(posDegrees);
+            // rotate our imageview
+
+            float percent=((posDegrees/maxDegree)*100);
+            setRotorPosAngle(getDegree(percent));
+            Log.e(TAG,"getDegree: "+getDegree(percent));
+
+            /*
                 // get a linear scale
                 float scaleDegrees = rotDegrees + 150; // given the current parameters, we go from 0 to 300
                 // get position percent
                 int percent = (int) (scaleDegrees / 3);
                 if (m_listener != null) m_listener.onRotate(percent);
+            */
                 return true; //consumed
            // } else
            //     return false;
         } else
             return false; // not consumed
+    }
+
+    private float getDegree(float percent){
+        Log.e(TAG,"percent: "+percent);
+        /**
+        initMaxDegree=(SweepAngle/2)
+        initMinDegree=(MaxDegree - (SweepAngle/2))
+        CurrentPercentageFromDegree=( (CurrentDegree*100) / SweepAngle)
+        CurrentConceptualDegreeFromPercentage=( (Percentage*SweepAngle) / 100)
+        CurrentRepresentationalDegree=( (initMinDegree+CurrentConceptualDegreeFromPercentage) % MaxDegree)
+         */
+        float currentConceptualDegreeFromPercentage=((percent*sweepAngle)/100);
+        return ((initMinDegree+currentConceptualDegreeFromPercentage)%maxDegree);
     }
 
     @Override
@@ -151,21 +246,7 @@ public class ACMeterView extends View implements OnGestureListener {
         //ivRotor.setImageBitmap(state?bmpRotorOn:bmpRotorOff);
     }
 
-    private void setupView(Context mContext, AttributeSet attrs) {
-        SetState(mState);
-        gestureDetector = new GestureDetector(mContext,this);
 
-        TypedArray a = mContext.getTheme().obtainStyledAttributes(attrs,
-                R.styleable.ACMeterView, 0, 0);
-        try {
-            value = a.getString(R.styleable.ACMeterView_value);
-            radius = a.getDimension(R.styleable.ACMeterView_radius, 20.0f);
-            meterColor = a.getColor(R.styleable.ACMeterView_meterColor, Color.BLACK);
-            meterValueTextColor = a.getColor(R.styleable.ACMeterView_textColor, Color.WHITE);
-        } finally {
-            a.recycle();
-        }
-    }
     /**
      * math..
      * @param x
@@ -220,6 +301,7 @@ public class ACMeterView extends View implements OnGestureListener {
         actualDraw(canvas);
     }
     void actualDraw(Canvas mCanvas){
+        float mRotationIndex=currentOffset-90;
 
         RectF mBaseRectangle=new RectF();
         float mOffset = .20f * radius;
@@ -233,7 +315,8 @@ public class ACMeterView extends View implements OnGestureListener {
 
         mBaseRectangle.set(mOffset, mOffset, (radius*2)-mOffset, (radius*2)-mOffset);
         Path mBasePath = new Path();
-        mBasePath.arcTo(mBaseRectangle, (290+20), (340), false);
+        mBasePath.arcTo(mBaseRectangle, 0, 359.9f, false);
+        //mBasePath.arcTo(mBaseRectangle, (290+20), (340), false);
 
 
         //Paint Setup - Base
@@ -246,7 +329,7 @@ public class ACMeterView extends View implements OnGestureListener {
 
 
         Path mColdPath = new Path();
-        mColdPath.arcTo(mBaseRectangle, 290, 20, false);
+        mColdPath.arcTo(mBaseRectangle, initMaxDegree+mRotationIndex, 20, false);
 
         //Paint Setup - Index
         Paint mPaintIndex=new Paint(Paint.ANTI_ALIAS_FLAG);
@@ -257,7 +340,7 @@ public class ACMeterView extends View implements OnGestureListener {
         mPaintIndex.setShader(new RadialGradient(getWidth()/2,getHeight()/2, radius-5,0xff3D8DD4,0xff4AAAFF,Shader.TileMode.CLAMP));
 
         Path mIndexPath = new Path();
-        mIndexPath.arcTo(mBaseRectangle, currentDegree-90, 2, false);
+        mIndexPath.arcTo(mBaseRectangle, 358, 2, false);
 
 
         //Paint Setup - Warm
@@ -269,31 +352,36 @@ public class ACMeterView extends View implements OnGestureListener {
         mPaintWarm.setShader(new RadialGradient(getWidth()/2,getHeight()/2, radius-5,0xffF20884,0xffF20884,Shader.TileMode.CLAMP));
 
         Path mWarmPath = new Path();
-        mWarmPath.arcTo(mBaseRectangle, 30, 10, false);
+        mWarmPath.arcTo(mBaseRectangle, initMinDegree+mRotationIndex, 10, false);
 
 
         //Canvas Draw
+
         mCanvas.drawPath(mBasePath,mPaintBase);
         mCanvas.drawPath(mColdPath,mPaintCold);
-        mCanvas.drawPath(mIndexPath,mPaintIndex);
         mCanvas.drawPath(mWarmPath,mPaintWarm);
+        mCanvas.drawPath(mIndexPath,mPaintIndex);
+    }
+    private void init(Context context,AttributeSet attrs){
+        gestureDetector = new GestureDetector(context,this);
+        initializeConstants(context,attrs);
+        initialCalculations();
     }
 
     public ACMeterView(Context context, AttributeSet attrs) {
         super(context, attrs);
-        setupView(context, attrs);
-        currentDegree=0;
+        init(context, attrs);
     }
 
     public ACMeterView(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-        setupView(context, attrs);
+        init(context, attrs);
 
     }
 
     @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
     public ACMeterView(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
-        setupView(context, attrs);
+        init(context, attrs);
     }
 }
